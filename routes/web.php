ca276336d8dc0c63eccd7ade9ee82df956e31e1c
asiripr <asiripramodaya2000@gmail.com>
2024-07-01 14:26:31 +0530

    created a stylesheet

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/StudentSavePage', function () {
+    return view('index');
+});
+
 Route::get('/test-01', function () {
     return 'this is test-01';
 });
